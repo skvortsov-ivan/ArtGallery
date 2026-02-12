334b44a973f7b041363e5ed9bc94ae0e7ec61b1e
skvortsov-ivan <ivan.skvortsov@chasacademy.se>
2026-02-12 19:24:13 +0100

    Added CI pipeline with a jest test to see if it works, test 3.

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello from vanilla JavaScript!');
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero');
+  }
+  return a / b;
+}
+
+module.exports = { add, subtract, multiply, divide };
