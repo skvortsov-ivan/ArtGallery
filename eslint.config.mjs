cab7349d37fbe8bc5a30990dac98a3f1073c3db0
skvortsov-ivan <ivan.skvortsov@chasacademy.se>
2026-02-12 19:10:04 +0100

    Added CI pipeline with a jest test to see if it works.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,22 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  { 
+    files: ["**/*.{js,mjs,cjs}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+        ...globals.node
+      }
+    } 
+  },
+  { 
+    files: ["**/*.js"], 
+    languageOptions: { 
+      sourceType: "script" 
+    } 
+  },
 ]);
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Art Gallery
+
+![CI](https://github.com/skvortsov-ivan/ArtGallery/actions/workflows/ci.yml/badge.svg)
+![codecov](https://codecov.io/gh/skvortsov-ivan/ArtGallery/branch/dev/graph/badge.svg)
